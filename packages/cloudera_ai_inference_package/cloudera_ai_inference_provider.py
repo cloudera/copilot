66756c377fc0e5e18166f9c3686a4254cdafcd2e
cl-gavan <gavan@cloudera.com>
2024-07-24 15:15:07 -0700

    DSE-37280, DSE-37389, DSE-37390, DSE-37386 - Add Support for Notebook… (#4)
    * DSE-37280, DSE-37389, DSE-37390, DSE-37386 - Add Support for Notebook Generation, Chat Completion API, /ask, and Persistent Chat History to AI Inference Copilot Models.
    
    * Address review comments.

--- packages/cloudera_ai_inference_package/cloudera_ai_inference_provider.py
+++ packages/cloudera_ai_inference_package/cloudera_ai_inference_provider.py
@@ -1,28 +1,21 @@
-from typing import Any, AsyncIterator, Dict, Iterator, List, Mapping, Optional
+import base64
 import json
 import logging
 import os
-import base64
+import requests
 import subprocess
-
-from langchain_core.language_models import LanguageModelInput
-from langchain_core.messages import BaseMessage, BaseMessageChunk
-from langchain_core.messages.ai import AIMessageChunk
-from langchain_core.outputs import ChatGenerationChunk
-from langchain_core.runnables.config import RunnableConfig
-
-def stringToB64String(s):
-    return base64.b64encode(s.encode('utf-8')).decode("utf-8")
-
-def b64StringToString(b):
-    return base64.b64decode(b).decode('utf-8')
+from typing import Any, AsyncIterator, Dict, Iterator, List, Mapping, Optional
 
 from jupyter_ai_magics import BaseProvider
 from jupyter_ai_magics.providers import MultiEnvAuthStrategy
 from langchain.callbacks.manager import CallbackManagerForLLMRun
 from langchain.chat_models.base import SimpleChatModel
 from langchain.llms.base import LLM
-import requests
+from langchain_core.language_models import LanguageModelInput
+from langchain_core.messages import BaseMessage, BaseMessageChunk, HumanMessage, SystemMessage
+from langchain_core.messages.ai import AIMessageChunk
+from langchain_core.outputs import ChatGenerationChunk
+from langchain_core.runnables.config import RunnableConfig
 
 def getCopilotModels(config_dir):
     if not config_dir or not os.path.exists(config_dir):
@@ -43,15 +36,15 @@ def getCopilotModels(config_dir):
     return ai_inference_models, models
 
 class ClouderaAIInferenceProvider(BaseProvider, SimpleChatModel, LLM):
-    api_key = "API_KEY2"
     id = "cloudera"
     name = "Cloudera AI Inference Provider"
-    model_id_key = "vllm_llama2"
     model = ""
+    model_id_key = ""
 
     copilot_config_dir = os.getenv("COPILOT_CONFIG_DIR") or ""
     ai_inference_models, models = getCopilotModels(copilot_config_dir)
-    cdp_cli_path = "/home/cdsw/.local/bin/cdp"
+    cdp_cli_path = "/Users/gavan/Library/Python/3.9/bin/cdp"
+    # "/home/cdsw/.local/bin/cdp"
 
     MultiEnvAuthStrategy(names=["CDP_PRIVATE_KEY", "CDP_ACCESS_KEY_ID", "CDP_REGION", "CDP_ENDPOINT_URL", "ENDPOINT_URL"])
 
@@ -95,7 +88,6 @@ class ClouderaAIInferenceProvider(BaseProvider, SimpleChatModel, LLM):
         super().__init__(**kwargs)
         self.model = kwargs.get("model_id")
         logging.basicConfig(filename="copilot.txt", level=logging.DEBUG, format="")
-        model = kwargs.get("model_id")
         cdp_private_key = os.getenv("CDP_PRIVATE_KEY")
         cdp_access_key_id = os.getenv("CDP_ACCESS_KEY_ID")
         cdp_region = os.getenv("CDP_REGION")
@@ -118,63 +110,169 @@ class ClouderaAIInferenceProvider(BaseProvider, SimpleChatModel, LLM):
                 return endpoint['endpoint']
         return ""
 
+    def BuildChatCompletionMessage(self, messages):
+        request_messages = []
+        for message in messages:
+            role = "assistant"
+            if isinstance(message, SystemMessage):
+                role = "system"
+            elif isinstance(message, HumanMessage):
+                role = "user"
+            request_messages.append({"role": role, "content": message.content})
+        return request_messages
+
+    def BuildCompletionPrompt(self, messages):
+        prompt = ""
+        for message in messages:
+            if isinstance(message, SystemMessage):
+                prompt += f"{message.content}\n"
+            elif isinstance(message, HumanMessage):
+                prompt += f"\nHuman: {message.content}"
+            else:
+                prompt += f"\nAI: {message.content}"
+        prompt += "\nAI: "
+        return prompt
 
     def _stream(
+        # Support streaming output tokens.
         self,
         messages: List[BaseMessage],
         stop: Optional[List[str]] = None,
         run_manager: Optional[CallbackManagerForLLMRun] = None,
-        **kwargs: Any) -> Iterator[ChatGenerationChunk]:
-
+        **kwargs: Any
+    ) -> Iterator[ChatGenerationChunk]:
         inference_endpoint = self.GetInferenceEndpoint(self.model)
         if not inference_endpoint:
             logging.error("Unable to find endpoint: " + self.model)
             return "Error: unable to find endpoint: " + self.model
-        req_data = '{"prompt": "' + messages[-1].content.encode('unicode_escape').decode("utf-8")
-
-        my_req_data = req_data + '","model":"' + self.model + '","temperature":1,"max_tokens":256,"stream":true}'
-        logging.info('req:')
-        logging.info(my_req_data)
 
         cdp_token = self.GetCDPToken()
         if not cdp_token:
             logging.error("Unable to get CDP Token.")
             return "Unable to get CDP Token. Please install CDP CLI by running the command 'pip install cdpcli' in Terminal Access and make sure all required environment variables are set. See https://docs.cloudera.com/r/jupyter-copilot for more details."
-        try:
-            r = requests.post(inference_endpoint,
-                              data = my_req_data,
-                              headers={'Content-Type': 'application/json',
-                              'Authorization': 'Bearer ' + cdp_token},
-                              verify=True,
-                              stream=True)
-        except Exception as e:
-            logging.error(e)
-            return "Request to Cloudera AI Inference Service failed."
 
-        for line in r.iter_lines():
-            if line:
-                decoded_line = line.decode('utf-8')
-                if decoded_line and len(decoded_line) > 6:
-                    data = decoded_line[6:]
-                    if data != "[DONE]":
-                        line_json = json.loads(decoded_line[6:])
-                        if "choices" in line_json:
-                            if "text" in line_json["choices"][0]:
-                                chunk = ChatGenerationChunk(message=AIMessageChunk(content=line_json["choices"][0]["text"]))
-                                yield chunk
+        if inference_endpoint.find("chat/completions") != -1:
+            # OpenAI Chat completions API
+            request_messages = self.BuildChatCompletionMessage(messages)
+
+            request = {"messages": request_messages, "model": self.model, "temperature": 1, "max_tokens": 256, "stream": True}
+            logging.info(f"request: {request}")
+            try:
+                r = requests.post(
+                    inference_endpoint,
+                    data=json.dumps(request),
+                    headers={'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + cdp_token},
+                    verify=True,
+                    stream=True
+                )
+            except Exception as e:
+                logging.error(e)
+                return "Request to Cloudera AI Inference Service failed."
+
+            for line in r.iter_lines():
+                decoded_line = line.decode('utf-8').removeprefix("data: ")
+                if decoded_line == "":
+                    continue
+                if decoded_line == "[DONE]":
+                    break
+                line_json = json.loads(decoded_line)
+                if "choices" in line_json and "delta" in line_json["choices"][0] and "content" in line_json["choices"][0]["delta"]:
+                    yield ChatGenerationChunk(message=AIMessageChunk(content=line_json["choices"][0]["delta"]["content"]))
+        elif inference_endpoint.find("completions") != -1:
+            # OpenAI Completions API
+            prompt = self.BuildCompletionPrompt(messages)
+            req_data = '{"prompt": "' + prompt.encode('unicode_escape').decode("utf-8")
+
+            my_req_data = req_data + '","model":"' + self.model + '","temperature":1,"max_tokens":256,"stream":true}'
+            logging.info('req:')
+            logging.info(my_req_data)
+
+            try:
+                r = requests.post(
+                    inference_endpoint,
+                    data = my_req_data,
+                    headers={'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + cdp_token},
+                    verify=True,
+                    stream=True
+                )
+            except Exception as e:
+                logging.error(e)
+                return "Request to Cloudera AI Inference Service failed."
+
+            for line in r.iter_lines():
+                decoded_line = line.decode('utf-8').removeprefix("data: ")
+                if decoded_line == "":
+                    continue
+                if decoded_line == "[DONE]":
+                    break
+                line_json = json.loads(decoded_line)
+                if "choices" in line_json and "text" in line_json["choices"][0]:
+                    yield ChatGenerationChunk(message=AIMessageChunk(content=line_json["choices"][0]["text"]))
 
     def _call(
+        # Supports notebook generation and magic commands.
         self,
-        prompt: str,
+        messages: List[BaseMessage],
         stop: Optional[List[str]] = None,
         run_manager: Optional[CallbackManagerForLLMRun] = None,
         **kwargs: Any,
     ) -> str:
-        logging.error("_call")
-        logging.error(prompt)
-        return prompt
+        inference_endpoint = self.GetInferenceEndpoint(self.model)
+        if not inference_endpoint:
+            logging.error("Unable to find endpoint: " + self.model)
+            return "Error: unable to find endpoint: " + self.model
+
+        cdp_token = self.GetCDPToken()
+        if not cdp_token:
+            logging.error("Unable to get CDP Token.")
+            return "Unable to get CDP Token. Please install CDP CLI by running the command 'pip install cdpcli' in Terminal Access and make sure all required environment variables are set. See https://docs.cloudera.com/r/jupyter-copilot for more details."
+
+        if inference_endpoint.find("chat/completions") != -1:
+            # OpenAI Chat completions API
+            request_messages = self.BuildChatCompletionMessage(messages)
+            request = {"messages": request_messages, "model": self.model, "temperature": 1, "max_tokens": 1024, "stream": False}
+            logging.info(json.dumps(request))
+            try:
+                r = requests.post(inference_endpoint,
+                                  data=json.dumps(request),
+                                  headers={'Content-Type': 'application/json',
+                                  'Authorization': 'Bearer ' + cdp_token},
+                                  verify=True,
+                                  stream=False)
+                rjson = r.json()
+                logging.info(f"rjson: {rjson}")
+                if rjson and rjson['choices'] and len(rjson['choices']) > 0 and rjson['choices'][0]['message'] and rjson['choices'][0]['message']['content']:
+                    return rjson['choices'][0]['message']['content']
+            except Exception as e:
+                logging.error(e)
+                return "Request to Cloudera AI Inference Service failed."
+        elif inference_endpoint.find("completions") != -1:
+            # OpenAI Completions API
+            prompt = self.BuildCompletionPrompt(messages)
+            logging.info(f"prompt: {prompt}")
+            request = {"prompt": prompt, "model": self.model, "temperature": 1, "max_tokens": 1024, "stream": False}
+            logging.info(json.dumps(request))
+
+            try:
+                r = requests.post(
+                    inference_endpoint,
+                    data = json.dumps(request),
+                    headers={'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + cdp_token},
+                    verify=True,
+                    stream=False
+                )
+                rjson = r.json()
+                logging.info(f"rjson: {rjson}")
+                if r.json() and rjson['choices'] and len(rjson['choices']) > 0 and rjson['choices'][0]['text']:
+                    return rjson['choices'][0]['text']
+            except Exception as e:
+                logging.error(e)
+                return "Request to Cloudera AI Inference Service failed."
 
     @property
     def _identifying_params(self) -> Mapping[str, Any]:
-        """Get the identifying parameters."""
-        return {"api_key": self.api_key}
+        # Get the identifying parameters.
+        return {}
