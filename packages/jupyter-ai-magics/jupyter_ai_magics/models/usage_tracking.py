52cb7734a80230d2f396314c626e7480ff3f51b1
cl-gavan <gavan@cloudera.com>
2024-08-27 15:09:39 -0700

    Fix cmlapi usage. (#19)

--- packages/jupyter-ai-magics/jupyter_ai_magics/models/usage_tracking.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/models/usage_tracking.py
@@ -2,26 +2,37 @@ import os
 from logging import Logger
 
 class FakeCMLAPI(object):
-    def send_copilot_event(req):
-        logger = Logger("copilot")
-        if "application_id" in req.keys():
-            logger.info(f"application_id: {req['application_id']}")
-        if "engine_id" in req.keys():
-            logger.info(f"engine_id: {UsageTracker.engine_id}")
-        if "event_details" in req.keys():
-            logger.info(f"event_details: {req['event_details']}")
-        if "event_type" in req.keys():
-            logger.info(f"event_type: {req['event_type']}")
-        if "include_selection" in req.keys():
-            logger.info(f"include_selection: {req['include_selection']}")
-        if "model_name" in req.keys():
-            logger.info(f"model_name: {req['model_name']}")
-        if "model_provider_id" in req.keys():
-            logger.info(f"model_provider_id: {req['model_provider_id']}")
-        if "model_type" in req.keys():
-            logger.info(f"model_type: {req['model_type']}")
-        if "prompt_word_count" in req.keys():
-            logger.info(f"prompt_word_count: {req['prompt_word_count']}")
+    """
+    Fake CML API client for testing purposes.  The real API call should look like this:
+
+    import cmlapi
+    api_client = cmlapi.default_client()
+    api_client.send_copilot_event(req)
+    """
+    class client(object):
+        def send_copilot_event(self, req):
+            logger = Logger("copilot")
+            if "application_id" in req.keys():
+                logger.info(f"application_id: {req['application_id']}")
+            if "engine_id" in req.keys():
+                logger.info(f"engine_id: {UsageTracker.engine_id}")
+            if "event_details" in req.keys():
+                logger.info(f"event_details: {req['event_details']}")
+            if "event_type" in req.keys():
+                logger.info(f"event_type: {req['event_type']}")
+            if "include_selection" in req.keys():
+                logger.info(f"include_selection: {req['include_selection']}")
+            if "model_name" in req.keys():
+                logger.info(f"model_name: {req['model_name']}")
+            if "model_provider_id" in req.keys():
+                logger.info(f"model_provider_id: {req['model_provider_id']}")
+            if "model_type" in req.keys():
+                logger.info(f"model_type: {req['model_type']}")
+            if "prompt_word_count" in req.keys():
+                logger.info(f"prompt_word_count: {req['prompt_word_count']}")
+
+    def default_client():
+        return FakeCMLAPI.client()
 
 try:
     import cmlapi
@@ -43,4 +54,5 @@ class UsageTracker(object):
 
     def _SendCopilotEvent(self,  req):
         req["engine_id"] = self.engine_id
-        cmlapi.send_copilot_event(req)
+        api_client = cmlapi.default_client()
+        api_client.send_copilot_event(req)
